5e83089814fe7bb302caca34ce94924647dc33c3
GABRIEL SMITH <Gabe@GABRIELs-MacBook-Pro.local>
2016-08-24 14:49:53 +0100

    created a combined gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,20 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
     browserify = require('gulp-browserify'),
+    compass = require('gulp-compass'),
     concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
+
 var jsSources = [
     'components/scripts/rclick.js',
     'components/scripts/pixgrid.js',
     'components/scripts/tagline.js',
     'components/scripts/template.js'
 ];
+
+
+var sassSources = ['components/sass/style.scss']
 
 gulp.task('coffee', function() {
     gulp.src(coffeeSources)
@@ -25,3 +30,17 @@ gulp.task('js', function() {
         .pipe(browserify())
         .pipe(gulp.dest('builds/development/js'))
 });
+
+gulp.task('compass', function() {
+    gulp.src(sassSources)
+        .pipe(compass({
+            sass: 'components/sass',
+            image: 'builds/development/images',
+            comments: true,
+            style: 'expanded'       
+            })
+        .on('error', gutil.log))
+        .pipe(gulp.dest('builds/development/css'))
+});
+
+gulp.task('all', ['coffee','js','compass']);
